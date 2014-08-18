520b750368506c9e396dd2ec0d6325cea9c60ee5
root <root@localhost>
2014-08-18 11:02:46 +0000

    Initial commit

---
+++ hackbig/settings.py
@@ -0,0 +1,64 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+SECRET_KEY = 'psqwvm4d=+b&l%$qr8ns@eigr6plhiz#r+x+osazc*cwys-c=f'
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rango',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'hackbig.urls'
+
+WSGI_APPLICATION = 'hackbig.wsgi.application'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+      '/root/hackbig/templates',)
+STATICFILES_DIRS = (
+  '/root/hackbig/static',)
